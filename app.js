12997b7df9dd0e6d924904bc08dbd5ea284d0e0c
Sam Genduso <sam.genduso@gmail.com>
2015-08-06 14:00:24 -0600

    minor code cleanup

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var moment = require('moment');
-// var session = require('express-session');
-// var flash = require('flash');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -25,18 +22,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(session());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(flash());
+
 
 app.use('/', routes);
 app.use('/users', users);
-//
-// app.use(function (req, res) {
-//   // flash a message
-//   req.flash('info', 'hello!');
-//   next();
-// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
